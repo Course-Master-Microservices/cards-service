6317cadfa9cfb47bfc2e01376986f5a50b8c8937
luisalho <luisalhopt@gmail.com>
2025-08-16 12:55:25 +0100

    Cards Service S2

---
+++ src/main/java/dev/alho/cards/CardsApplication.java
@@ -0,0 +1,40 @@
+package dev.alho.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAware")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Bank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "John Doe",
+						email = "tutor@alho.dev",
+						url = "https://www.alho.dev"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.alho.dev"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Cards microservice REST API Documentation",
+				url = "https://www.alho.dev/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
